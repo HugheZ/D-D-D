cf95e1254e79122b5e6e67cc20d54176dc2a8f01
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2020-02-25 19:36:40 -0500

    Shuffle and finish buttons
    Added shuffle and finish buttons and other minor UI things to the branch. Currently just tracks to make sure there are 3 shuffles, but doesnt actually shuffle anything yet

--- Assets/Scripts/DungeonBuilderManager.cs
+++ Assets/Scripts/DungeonBuilderManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DungeonBuilderManager : MonoBehaviour {
 
     public GameObject cursor;
     public GameObject room;
+    public Button shuffleButton;
+    public Text shuffleText;
+    int shufflesLeft;
 
     //stuff for Singleton
     private static DungeonBuilderManager _instance = null;
@@ -27,8 +31,9 @@ public class DungeonBuilderManager : MonoBehaviour {
     }
     // Use this for initialization
     void Start () {
-		
-	}
+        shufflesLeft = 3;
+        shuffleText.text = "Shuffles Left: " + shufflesLeft;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -38,5 +43,20 @@ public class DungeonBuilderManager : MonoBehaviour {
     public Transform GetRoomTransform()
     {
         return room.transform;
+    }
+
+    public void ShuffleNewItems()
+    {
+        shufflesLeft--;
+        shuffleText.text = "Shuffles Left: " + shufflesLeft;
+        if (shufflesLeft == 0)
+        {
+            shuffleButton.enabled = false;
+            shuffleButton.image.color = Color.red;
+        }
+    }
+    public void FinishRoom()
+    {
+
     }
 }

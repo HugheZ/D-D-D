450830a55ebdce600dc118a832eef2452a56bd99
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2020-03-04 13:31:41 -0500

    new scene
    added a new scene for the actual multiplayer run of dungeon rooms

--- Assets/Scripts/DungeonBuilderManager.cs
+++ Assets/Scripts/DungeonBuilderManager.cs
@@ -88,6 +88,13 @@ public class DungeonBuilderManager : MonoBehaviour {
         fire3.GetComponent<SpriteRenderer>().color = fireColor;
         GameObject fire4 = Instantiate(fire, new Vector2(-2.5f, 4.5f), Quaternion.identity, room.transform);
         fire4.GetComponent<SpriteRenderer>().color = fireColor;
-        //SceneManager.LoadScene("MainMenu");
+        //TODO: Set tag of room to be correct player's tag
+        room.tag = "P1Room";
+        //room.tag = "P2Room";
+        //room.tag = "P3Room";
+        //room.tag = "P4Room";
+        //preserve the room by not destroying it
+        DontDestroyOnLoad(room);
+        SceneManager.LoadScene("MultiplayerRun");
     }
 }

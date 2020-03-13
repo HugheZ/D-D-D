970b15121823e58eee2a92b34121c929f449ef64
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2020-03-13 17:53:01 -0400

    Overhaul beginnings
    Manager has a list of rooms to grab from. spawns one at each player area

--- Assets/Scripts/MultiplayerRunManager.cs
+++ Assets/Scripts/MultiplayerRunManager.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class MultiplayerRunManager : MonoBehaviour {
 
-    public GameObject p1room;
-    public GameObject p2room;
-    public GameObject p3room;
-    public GameObject p4room;
+    public List<GameObject> possibleRoomList;
     public GameObject bossRoom;
     public GameObject boss;
     bool bossSpawned;
@@ -18,48 +15,17 @@ public class MultiplayerRunManager : MonoBehaviour {
     public Transform p4Space;
     public List<Transform> playerSpawns;
     public Camera p1camera, p2camera, p3camera, p4camera;
+    System.Random rnd;
     
 
 	// Use this for initialization
 	void Start () {
-        if (p1room == null)
-        {
-            p1room = GameObject.FindGameObjectWithTag("P1Room");
-            ArrowTrapScript[] arrowTraps = (ArrowTrapScript[])FindObjectsOfType(typeof(ArrowTrapScript));
-            foreach(ArrowTrapScript arrowTrap in arrowTraps)
-                arrowTrap.SetEnabled();
-            SawbladeScript[] sawblades = (SawbladeScript[])FindObjectsOfType(typeof(SawbladeScript));
-            foreach (SawbladeScript sawblade in sawblades)
-                sawblade.FlipActive();
-            p1room.SetActive(false);
-            p1room.transform.position = new Vector2(0, 0);
-        }
-        /*if (p2room == null)
-        {
-            p2room = GameObject.FindGameObjectWithTag("P2Room");
-            p2room.SetActive(false);
-        }
-        if (p3room == null)
-        {
-            p3room = GameObject.FindGameObjectWithTag("P3Room");
-            p3room.SetActive(false);
-        }
-        if (p4room == null)
-        {
-            p4room = GameObject.FindGameObjectWithTag("P4Room");
-            p4room.SetActive(false);
-        }*/
-
-        //start in rooms
-        GameObject p1r1 = Instantiate(p1room, p1Space);
-        GameObject p2r1 = Instantiate(p1room, p2Space);
-        GameObject p3r1 = Instantiate(p1room, p3Space);
-        GameObject p4r1 = Instantiate(p1room, p4Space);
-        
-        p1r1.SetActive(true);
-        p2r1.SetActive(true);
-        p3r1.SetActive(true);
-        p4r1.SetActive(true);
+        rnd = new System.Random();
+        GameObject startRoom = possibleRoomList[rnd.Next(0,possibleRoomList.Count)];
+        GameObject p1r1 = Instantiate(startRoom, p1Space);
+        GameObject p2r1 = Instantiate(startRoom, p2Space);
+        GameObject p3r1 = Instantiate(startRoom, p3Space);
+        GameObject p4r1 = Instantiate(startRoom, p4Space);
 
         bossSpawned = false;
 

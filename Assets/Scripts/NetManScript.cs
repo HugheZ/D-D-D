6e818a766fbdc7885193c9a4203858b374c6ef55
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2020-03-17 18:37:00 -0400

    NetMan added proper
    Found the custom netman script with a player map and added it. Added networkStartPosition components to the multiplayer player spawn points. Added network identities to basically every prefab.

---
+++ Assets/Scripts/NetManScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetManScript : NetworkManager
+{
+    public Dictionary<int, GameObject> playerMap;
+    NetworkManager myNetMan;
+
+    private static NetManScript _instance = null;
+    public static NetManScript Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        myNetMan = GetComponent<NetworkManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        //edit player
+        playerMap.Add(conn.connectionId, player);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+    public override void OnServerRemovePlayer(NetworkConnection conn, PlayerController player)
+    {
+        base.OnServerRemovePlayer(conn, player);
+    }
+
+
+}
